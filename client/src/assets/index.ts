ff73db814dfedfe42532d2e5cbed06a1e5c6e405
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2024-02-25 19:28:57 +0200

    QUECK_COMMIT

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,2 @@
-import _404Page from "./images/404.png";
-import userLogo from "./images/userLogo.png";
 import NotFoundCover from "./images/not-found.png";
-
-export { _404Page, userLogo, NotFoundCover };
+export { NotFoundCover };
