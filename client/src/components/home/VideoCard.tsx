709a5cc829f8cc91f84c6bdda02ad7079b064d9e
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2024-02-25 03:21:40 +0200

    QUECK_COMMIT

--- client/src/components/home/VideoCard.tsx
+++ client/src/components/home/VideoCard.tsx
@@ -13,15 +13,17 @@ export const VideoCard = ({ title, url, duration, quality, downloadedUrl, thumbn
    return (
       <div className="flex-between bg-gradient my-10 flex-col rounded-md p-2 shadow-md shadow-dimWhite md:flex-row">
          <div className="flex w-full flex-col items-start md:flex-row">
-            <div className="relative m-auto h-full">
+            <div className="relative mr-3 h-full">
                <img
                   src={thumbnail.url}
                   alt="img"
                   className="rounded-md"
                   style={{ width: thumbnail.width, height: thumbnail.height }}
                />
-               <Typography className="absolute left-1 top-1 rounded-sm bg-black text-white">{quality}</Typography>
-               <Typography className="absolute bottom-1 right-1 rounded-sm bg-black text-white">
+               <Typography className="absolute left-1 top-1 rounded-sm bg-black px-1 pb-1 text-base leading-none text-white">
+                  {quality}
+               </Typography>
+               <Typography className="absolute bottom-1 right-1 rounded-sm bg-black px-1 pb-1.5 text-base text-white">
                   {duration}
                </Typography>
             </div>
@@ -29,7 +31,7 @@ export const VideoCard = ({ title, url, duration, quality, downloadedUrl, thumbn
             <div className="w-full overflow-x-hidden">
                <Typography>{title}</Typography>
                <Typography className="my-3">{quality}</Typography>
-               <Typography className="text-sm text-dimWhite hover:text-black dark:hover:text-white">
+               <Typography className="w-fit text-sm text-dimWhite hover:text-black dark:hover:text-white">
                   <a href={url} target="_blank" rel="noopener noreferrer">
                      {url}
                   </a>
@@ -37,7 +39,7 @@ export const VideoCard = ({ title, url, duration, quality, downloadedUrl, thumbn
             </div>
          </div>
 
-         <Button icon="fa-download text-white group-hover:text-white" className="justify-self-end">
+         <Button icon="fa-download text-white group-hover:text-white">
             <a href={downloadedUrl} className="h-full w-full" target="_blank" download rel="noopener noreferrer">
                Download
             </a>
